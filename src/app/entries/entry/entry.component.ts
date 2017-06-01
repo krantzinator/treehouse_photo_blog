f848eb8793cd5fa6f5014c60ce574d83b59e0f30
krantzinator <krantzrs8@gmail.com>
2017-05-31 23:21:19 -0400

    resolved janky package.json versioning, plus component stuff

--- src/app/entries/entry/entry.component.ts
+++ src/app/entries/entry/entry.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { Entry } from '../shared/entry.model';
 
 @Component({
     selector: 'app-entry',
@@ -7,12 +8,5 @@ import { Component } from '@angular/core';
 })
 
 export class EntryComponent {
-    title: string = 'My First Photo';
-    photo: string = 'http://placehold.it/800x500?text=Angular Basics';
-    description: string = 'A Description of My First Photo';
-    comments: any[] = [
-        {name: "Jojo", comment: "Dilly-dally"},
-        {name: "Ohjo", comment: "Willy-wally"},
-        {name: "Scarjo", comment: "Shilly-shally"}
-    ];
+    @Input() entry: Entry;
 }
